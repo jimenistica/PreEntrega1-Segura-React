eb4c453b48011337a9f7cb537b4528b6f02c1c57
Jimena Segura <jimensitica@gmail.com>
2024-10-01 22:15:10 -0300

    images carrusel arreglada

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -11,13 +11,13 @@ import  Spinner from "../../components/Spinner";
 const Home = () => {
 
    const images = [
-    {src:'../../../public/images/carrusel-1.jpg',
+    {src:'/images/carrusel-1.jpg',
       caption: {text: 'Vestí tu cama con <strong>las mejores telas</strong>.'}
     },
-    {src:'../../../public/images/carrusel-2.jpg',
+    {src:'/images/carrusel-2.jpg',
       caption: {text: 'Llevá tu bebida <strong>donde quieras</strong>, sin fugas.'}
     },
-    {src:'../../../public/images/carrusel-3.jpg',
+    {src:'/images/carrusel-3.jpg',
       caption: {text: 'Los mejores <strong>equipos de mate</strong> para afrontar <strong>mejor</strong> los días.'}
     }, 
    ];
